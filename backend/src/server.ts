a3d8809d4d1c8772d88ddd111134bf8d36acbdbd
Icaro <icarorangel05@gmail.com>
2022-02-10 16:54:46 -0300

    create files typeorm

---
+++ backend/src/server.ts
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, () => console.log("Servidor rodando local na porta 3000"));
+
+app.get("/", function (req, res) {
+  res.send("<h1>Servidor rodando</h1>");
+});
