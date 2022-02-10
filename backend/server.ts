4709d20b43d26e895d8e5ea34d51b8112207910a
Icaro <icarorangel05@gmail.com>
2022-02-10 16:14:48 -0300

    install typeorm

--- backend/server.ts
+++ backend/server.ts
@@ -6,5 +6,5 @@ const app = express();
 app.listen(3000, () => console.log("Servidor rodando local na porta 3000"));
 
 app.get("/", function (req, res) {
-  res.send("<h1>Servidor rodando com ExpressJS</h1>");
+  res.send("<h1>Servidor rodando</h1>");
 });
