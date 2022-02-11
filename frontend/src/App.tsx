23ba0eadb93bbd91c1cca94d1f45d6172c07ff8b
Icaro <icarorangel05@gmail.com>
2022-02-11 15:12:49 -0300

    create page-users

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Header from "./components/Header";
+import Users from "./pages/Users";
 
 function App() {
-  return <Header />;
+  return <Users />;
 }
 
 export default App;
---
+++ frontend/src/pages/Users/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../components/Header";
+import { Container } from "./styles";
+
+const Users = () => {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+};
+
+export default Users;
