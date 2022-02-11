b6aca86c2e83af3437d29659f98cf13d34eec740
Icaro <icarorangel05@gmail.com>
2022-02-11 15:51:16 -0300

    create styles global

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routings from "./routes";
+import GlobalStyle from "./styles/global";
 
 function App() {
   return (
     <>
       <Router>
         <Routings />
+        <GlobalStyle />
       </Router>
     </>
   );
