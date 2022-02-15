7e65f160b65a86726d97691c981dd42eeaffd8c4
Icaro <icarorangel05@gmail.com>
2022-02-15 15:06:58 -0300

    install react-icons

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { Container } from "./styles";
+//@ts-ignore
+import { ReactComponent as Copyright } from "../../assets/copyright.svg";
+import { FiInstagram } from "react-icons/fi";
 
 const Footer = () => {
-  return <Container>Footer</Container>;
+  return (
+    <Container>
+      <Copyright />
+    </Container>
+  );
 };
 
 export default Footer;
--- frontend/src/components/Footer/styles.ts
+++ frontend/src/components/Footer/styles.ts
@@ -1,3 +1,11 @@
 import styled from "styled-components";
 
-export const Container = styled.footer``;
+export const Container = styled.footer`
+  display: flex;
+  align-items: center;
+  bottom: 0;
+  position: absolute;
+  width: 100%;
+  height: 40px;
+  border: 1px solid black;
+`;
