e81a8bdc103cf77164408b0d6f348a229f92b4c6
Icaro <icarorangel05@gmail.com>
2022-03-03 10:49:10 -0300

    responsive create, delete and update patient

--- frontend/src/pages/UpdatePatient/styles.ts
+++ frontend/src/pages/UpdatePatient/styles.ts
@@ -7,4 +7,24 @@ export const Container = styled.div`
     margin-top: 8px;
     margin-bottom: 18px;
   }
+  @media (max-width: 768px) {
+    h1 {
+      font-size: 16px !important;
+      width: 75%;
+    }
+    input {
+      height: 30px !important;
+      font-size: 12px;
+    }
+  }
+  @media (max-width: 400px) {
+    h1 {
+      font-size: 12px !important;
+      width: 75%;
+    }
+    input {
+      height: 30px !important;
+      font-size: 10px;
+    }
+  }
 `;
